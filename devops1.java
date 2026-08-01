77eca2e33e552776178818598acab8793e6d5c6f
meetp47 <patelmeet4477@gmail.com>
2026-08-01 10:35:11 +0530

    Update devops1.java

--- devops1.java
+++ devops1.java
@@ -1,5 +1,7 @@
+import java.util.*;
+
 public class devops1 {
-  public static void main(String[]args) {
+  public static void main(String[] args) {
     System.out.println("Hello Devops!");
   }
 } 
